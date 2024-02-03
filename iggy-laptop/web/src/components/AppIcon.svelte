42605b04b27a1a58f7a6cc4283abd082caba71ec
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-02-03 00:58:09 +0000

    fix(Laptop): Fixed AppIcons

--- iggy-laptop/web/src/components/AppIcon.svelte
+++ iggy-laptop/web/src/components/AppIcon.svelte
@@ -4,11 +4,12 @@
     export let name: string;
     export let icon: string;
     export let onClick: (this: Window, ev: MouseEvent) => any;
-    export let colour: string;
+    export let colour: string = "indigo-500";
+    console.log(colour);
 </script>
 
 <button
-    class={`w-24 aspect-square p-3 flex flex-col content-around outline outline-0 rounded-md hover:outline-2 text-${colour} bg-gray-800 my-1`}
+    class={`w-24 aspect-square p-3 flex flex-col content-around outline outline-0 rounded-md hover:outline-2 ${colour}  bg-gray-800 my-1`}
     on:click|preventDefault={onClick}
 >
     <Icon {icon} class="w-full h-1/2" />
