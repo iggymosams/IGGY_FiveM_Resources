d40fa39ede915130d1151f647b8d52a1aebd299c
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-02-05 10:09:41 +0000

    fix(admin/ui): Fixed padding

--- iggy-admin/web/src/components/AdminMenu/Commands/TopButtonGroup.svelte
+++ iggy-admin/web/src/components/AdminMenu/Commands/TopButtonGroup.svelte
@@ -12,7 +12,7 @@
         <button
             class={`h-full flex-auto ${
                 selected === tab ? "bg-rose-700" : "bg-neutral-800"
-            } text-white p-3 transition-colors duration-50`}
+            } text-white px-1 py-2 transition-colors duration-50`}
             on:click|preventDefault={() => updateSelected(tab)}
         >
             {tab}
