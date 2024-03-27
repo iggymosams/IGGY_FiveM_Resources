c6907c77d109ff87393e48bb8245f1552f9cd040
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-27 14:18:53 +0000

    feat(laptop/ui): Created home app

---
+++ iggy-laptop/web/src/apps/home/components/Home.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import AppBase from "../../../components/AppBase.svelte";
+    import AppIcon from "../../../components/AppIcon.svelte";
+    import { getApps } from "../../../utils/apps";
+
+    const { apps } = getApps();
+    console.log(apps);
+</script>
+
+<AppBase class="text-white">
+    <div class="grid grid-cols-12 gap-2 p-2">
+        {#each apps as app}
+            <AppIcon {app} />
+        {/each}
+    </div>
+</AppBase>
