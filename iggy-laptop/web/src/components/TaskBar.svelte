606eba62d5a3c677391259c9a379b5bb30ceaab7
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-27 16:13:22 +0000

    feat(laptop/ui): Updated taskbar

--- iggy-laptop/web/src/components/TaskBar.svelte
+++ iggy-laptop/web/src/components/TaskBar.svelte
@@ -1,3 +1,15 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import { Pyramid } from "lucide-svelte";
+    import DateTime from "./DateTime.svelte";
+</script>
 
-<div class="bg-neutral-700 w-full h-10"></div>
+<div
+    class="backdrop-blur-xl bg-opacity-50 bg-neutral-700 w-full h-10 flex items-center px-2"
+>
+    <button
+        class="text-blue-400 p-1.5 rounded-sm hover:bg-neutral-500/20 transition-colors"
+    >
+        <Pyramid />
+    </button>
+    <DateTime />
+</div>
