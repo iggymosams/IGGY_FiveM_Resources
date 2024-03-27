577802f1d68795d65d0df53395215959687904be
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-27 16:12:39 +0000

    feat(laptop/ui): Created DateTime

---
+++ iggy-laptop/web/src/components/DateTime.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let time = new Date();
+
+    $: hours = time.getHours().toString().padStart(2, "0");
+    $: minutes = time.getMinutes().toString().padStart(2, "0");
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            time = new Date();
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    });
+</script>
+
+<div class="ml-auto text-end text-xs p-2 text-white">
+    <div>{hours}:{minutes}</div>
+    <div>27/03/2024</div>
+</div>
