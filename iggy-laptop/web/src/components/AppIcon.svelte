3988eca994e2e9abb814f9dc7f39ac240ca769e0
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-27 14:19:31 +0000

    feat(laptop/ui): Created AppIcon

---
+++ iggy-laptop/web/src/components/AppIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Link } from "svelte-routing";
+    import type { ConfigApp } from "../utils/apps";
+
+    export let app: ConfigApp;
+</script>
+
+<Link to={app.path}>
+    <div
+        class={`aspect-square ${app.backgroundColor} ${app.color} rounded-md flex items-center justify-center flex-col`}
+    >
+        <svelte:component this={app.icon} />
+        <span class={app.color}>{app.name}</span>
+    </div>
+</Link>
