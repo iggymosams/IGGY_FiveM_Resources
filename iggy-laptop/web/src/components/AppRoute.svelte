7d454df061cc83bc51e26009c77deb88115badf3
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-27 14:19:42 +0000

    feat(laptop/ui): Created AppRoute

---
+++ iggy-laptop/web/src/components/AppRoute.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { SvelteComponent } from "svelte";
+    import { Route } from "svelte-routing";
+    export let component: typeof SvelteComponent;
+    export let id: string;
+    export let path: string;
+</script>
+
+<Route {path}>
+    <svelte:component this={component} {id} />
+</Route>
