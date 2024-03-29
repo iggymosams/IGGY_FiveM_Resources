72dc6c4258719e0f4f13e1ab61e1e977193b68c7
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-29 15:08:59 +0000

    feat(laptop/ui): Added handle and VPN display

--- iggy-laptop/web/src/components/DateTime.svelte
+++ iggy-laptop/web/src/components/DateTime.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
+    import { Wifi, GlobeLock } from "lucide-svelte";
     import { onMount } from "svelte";
+    import { handle, hasVPN } from "../store/stores";
+    import { fade } from "svelte/transition";
+
+    let hover = false;
 
     let time = new Date();
 
@@ -17,7 +22,33 @@
     });
 </script>
 
-<div class="ml-auto text-end text-xs p-2 text-white">
-    <div>{hours}:{minutes}</div>
-    <div>27/03/2024</div>
+<div class="ml-auto flex h-full items-center text-white">
+    {#if $hasVPN}
+        <div
+            class="relative flex justify-center"
+            on:mouseenter={() => (hover = true)}
+            on:mouseleave={() => (hover = false)}
+            role="dialog"
+        >
+            <GlobeLock />
+            {#if hover}
+                <div
+                    class="w-max absolute bottom-[100%] transition-all"
+                    transition:fade={{ duration: 250 }}
+                >
+                    <div
+                        class="rounded-md p-2 mb-[10px] backdrop-blur-xl bg-opacity-50 bg-neutral-700"
+                    >
+                        Logged in as {$handle}
+                    </div>
+                </div>
+            {/if}
+        </div>
+    {:else}
+        <Wifi />
+    {/if}
+    <div class="text-end text-xs p-2">
+        <div>{hours}:{minutes}</div>
+        <div>27/03/2024</div>
+    </div>
 </div>
