98bb6ff4fbc99662538bc2e3136ee3b38726b6f9
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-29 13:54:55 +0000

    feat(laptop): Updated to use new functions

--- iggy-laptop/server/server.ts
+++ iggy-laptop/server/server.ts
@@ -1,13 +1,11 @@
 import { Player, Server } from "@zerio2/qbcore.js";
 import "./sv_handle";
+import "./sv_exports";
+import { GetHandle } from "./sv_handle";
+import { HasVPN } from "./sv_exports";
 
-const QBCore: Server = global.exports["qb-core"].GetCoreObject();
+export const QBCore: Server = global.exports["qb-core"].GetCoreObject();
 
-QBCore.Functions.CreateUseableItem("laptop", (src, item) => {
-    let player: Player = QBCore.Functions.GetPlayer(src);
-    let vpn = player.Functions.GetItemByName("vpn");
-    let hasVPN = vpn !== undefined;
-    let handle = undefined;
-    if (hasVPN) handle = vpn.info.handle;
-    emitNet("iggy-laptop:client:open", src, hasVPN, handle);
+QBCore.Functions.CreateUseableItem("laptop", async (src, item) => {
+    emitNet("iggy-laptop:client:open", src, HasVPN(src), await GetHandle(src));
 });
