4b3ff80cf593073e912d99b5a5e44d2ff53b5936
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-29 02:03:34 +0000

    feat(laptop): Added cl_utils

---
+++ iggy-laptop/client/cl_utils.ts
@@ -0,0 +1,22 @@
+import { Client } from "@zerio2/qbcore.js";
+
+const QBCore: Client = global.exports["qb-core"].GetCoreObject();
+
+async function TriggerQBCallBack(name: string, ...args: any[]) {
+    try {
+        let data = await new Promise<any>((resolve, reject) => {
+            QBCore.Functions.TriggerCallback(
+                name,
+                (result: any) => {
+                    resolve(result);
+                },
+                args
+            );
+        });
+        return data;
+    } catch (error) {
+        return;
+    }
+}
+
+export { TriggerQBCallBack };
