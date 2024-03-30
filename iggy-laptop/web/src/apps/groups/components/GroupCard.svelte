68c145913d691736ec77967aa6e70b548e154d4b
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-03-30 23:31:27 +0000

    feat(laptop/ui): Created groups app

---
+++ iggy-laptop/web/src/apps/groups/components/GroupCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { fetchNui } from "../../../utils/fetchNui";
+
+    export let leader: string;
+    export let players: number;
+    export let id: number;
+</script>
+
+<button
+    class="flex justify-between w-full p-4 bg-zinc-700 rounded-md hover:bg-zinc-600 transition-colors"
+    on:click={() => fetchNui("groups:requestGroup", id)}
+>
+    <div class="font-bold">{leader}</div>
+    <div>{players + 1}/4</div>
+</button>
