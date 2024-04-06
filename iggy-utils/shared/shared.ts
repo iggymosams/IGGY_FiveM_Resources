b35df050370b0a3836195541d7d7c760bd035415
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-06 21:39:48 +0100

    feat(utils): Created iggy-utils

---
+++ iggy-utils/shared/shared.ts
@@ -0,0 +1,27 @@
+function Delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+global.exports("Delay", Delay);
+
+function CalcDist(
+    start_x: number,
+    start_y: number,
+    start_z: number,
+    target_x: number,
+    target_y: number,
+    target_z: number
+) {
+    var a = start_x - target_x;
+    var b = start_y - target_y;
+    var c = start_z - target_z;
+
+    return Math.sqrt(a * a + b * b + c * c);
+}
+global.exports("CalcDist", CalcDist);
+
+function RandomNumber(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+}
+global.exports("RandomNumber", RandomNumber);
+
+console.log("Shared Utils");
