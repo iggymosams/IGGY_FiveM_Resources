22c89711a1053714435ca9fe99b12314d283653a
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-06 21:53:17 +0100

    feat(utils): Added TriggerQBCallBack

--- iggy-utils/client/client.ts
+++ iggy-utils/client/client.ts
@@ -1,1 +1,22 @@
 import "../shared/shared";
+import { Client } from "@zerio2/qbcore.js";
+
+const QBCore: Client = global.exports["qb-core"].GetCoreObject();
+
+async function TriggerQBCallBack(name: string, ...args: any[]) {
+    try {
+        let data = await new Promise<any>((resolve, reject) => {
+            QBCore.Functions.TriggerCallback(
+                name,
+                (result: any) => {
+                    resolve(result);
+                },
+                args
+            );
+        });
+        return data;
+    } catch (error) {
+        return;
+    }
+}
+global.exports("TriggerQBCallBack", TriggerQBCallBack);
