97a3c822b1c1a1a034dcdb4d8c407cf88741b2c8
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 16:09:13 +0100

    feat(laptop/ui): Added client loading

--- iggy-laptop/web/src/apps/ridge/utils.ts
+++ iggy-laptop/web/src/apps/ridge/utils.ts
@@ -6,38 +6,42 @@ import { get } from "svelte/store";
 
 export async function searchURL(url: string, tab: string) {
     progressBar();
+    let pageData = undefined;
+
     try {
         let resp = await fetchNui("ridge:searchURL", url);
+        pageData = resp;
     } catch (error) {
         let example =
             '{"title":"IGGYMOSAMS","url":"iggymosams.dev","page":{"template":"Test","data":{"input1":"Welcome to IGGYMOSAMS.dev"}}}';
 
-        let parsedExample = JSON.parse(example);
-
-        let page: typeof SvelteComponent<any> | undefined = undefined;
-        switch (parsedExample.page.template) {
-            case "Test":
-                page = Test;
-                break;
-
-            default:
-                break;
-        }
-
-        if (page === undefined) return;
-
-        tabs.update((currentTabs) => {
-            let i = currentTabs.findIndex((t) => t.id === tab);
-            currentTabs[i].title = parsedExample.title;
-            currentTabs[i].url = parsedExample.url;
-            currentTabs[i].page.content = page;
-            currentTabs[i].page.data = parsedExample.page.data;
-
-            return currentTabs;
-        });
-
-        activeTab.set(get(tabs).find((t) => t.id === tab) || get(tabs)[0]);
+        pageData = JSON.parse(example);
     }
+
+    let page: typeof SvelteComponent<any> | undefined = undefined;
+    switch (pageData.page.template) {
+        case "Test":
+            page = Test;
+            break;
+
+        default:
+            break;
+    }
+
+    if (page === undefined) return;
+
+    tabs.update((currentTabs) => {
+        let i = currentTabs.findIndex((t) => t.id === tab);
+        currentTabs[i].title = pageData.title;
+        currentTabs[i].url = pageData.url;
+        currentTabs[i].page.content = page;
+        currentTabs[i].page.data = pageData.page.data;
+
+        return currentTabs;
+    });
+
+    activeTab.set(get(tabs).find((t) => t.id === tab) || get(tabs)[0]);
+
     progress.set(100);
     setTimeout(() => progress.set(0), 2000);
 }
