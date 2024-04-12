fa8a0349c09a77dca235542bbf424b760c492140
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 14:13:44 +0100

    feat(laptop/ui): Created Test template

---
+++ iggy-laptop/web/src/apps/ridge/components/templates/Test.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let data: { [key: string]: string };
+    let input1Value = data["input1"] || "Welcome to Ridge Browser";
+</script>
+
+<div
+    class="w-full h-full flex flex-col items-center justify-center gap-2 bg-neutral-600"
+>
+    <div contenteditable={false}>{input1Value}</div>
+
+    {JSON.stringify(data)}
+</div>
