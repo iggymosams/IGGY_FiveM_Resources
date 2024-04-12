fd17d38e2f4eaa991e7d5eedbc1819df9a079978
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 02:31:38 +0100

    feat(laptop/ui): Created ToolbarButton

---
+++ iggy-laptop/web/src/apps/ridge/components/ToolbarButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function handleClick() {
+        dispatch("click");
+    }
+</script>
+
+<button class="hover:bg-neutral-600 p-1 rounded-md" on:click={handleClick}>
+    <slot />
+</button>
