e00a3695fda977761e12f0b8b9acea092eac92d0
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 15:07:44 +0100

    feat(laptop/ui): Created template with floating button and page saving

---
+++ iggy-laptop/web/src/apps/ridge/components/Template.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { fetchNui } from "../../../utils/fetchNui";
+    import { activeTab, editing, tabs } from "../../../store/ridge";
+    import { Pencil } from "lucide-svelte";
+    import type { PageData } from "../types";
+
+    export let pageData: PageData;
+
+    let isOwner = false;
+
+    onMount(async () => {
+        $editing = false;
+        try {
+            let owner = await fetchNui("ridge:getSiteOwner", $activeTab.url);
+            isOwner = owner;
+        } catch (error) {
+            isOwner = true;
+        }
+    });
+
+    function saveData() {
+        let tabId = $activeTab.id;
+
+        tabs.update((currentTabs) => {
+            let i = currentTabs.findIndex((t) => t.id === tabId);
+
+            currentTabs[i].page.data = pageData;
+
+            return currentTabs;
+        });
+        activeTab.set($tabs.find((t) => t.id === tabId) || $tabs[0]);
+        fetchNui("ridge:saveSite", { pageData, $activeTab });
+    }
+</script>
+
+<slot />
+{#if isOwner}
+    <div class="absolute bottom-0 right-0 p-5 w-min">
+        <button
+            class="p-3 bg-orange-400 rounded-full shadow-lg text-white hover:bg-orange-500"
+            on:click={() => {
+                editing.set(!$editing);
+                saveData();
+            }}
+        >
+            <Pencil />
+        </button>
+    </div>
+{/if}
