50db7a2eeac0490ecc23aa291da90c9d98909a74
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 02:31:15 +0100

    feat(laptop/ui): Created ridge types

---
+++ iggy-laptop/web/src/apps/ridge/types.ts
@@ -0,0 +1,12 @@
+import type { SvelteComponent } from "svelte";
+
+export interface Tab {
+    id: string;
+    title: string;
+    page: Page;
+    url?: string;
+}
+
+export interface Page {
+    content: typeof SvelteComponent<any>;
+}
