982454fcd29dee786e97b8bcd7a622b2b4c4ce77
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 13:50:03 +0100

    feat(laptop/ui): Added RandomID to misc

--- iggy-laptop/web/src/utils/misc.ts
+++ iggy-laptop/web/src/utils/misc.ts
@@ -1,1 +1,8 @@
 export const isEnvBrowser = (): boolean => !(window as any).invokeNative;
+
+export function randomID() {
+    return (
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15)
+    );
+}
