69ce627bd8811918e21a30672f71f04463f340e1
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 02:32:17 +0100

    feat(laptop/ui): Created Toolbar

---
+++ iggy-laptop/web/src/apps/ridge/components/Toolbar.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+    import {
+        ArrowLeft,
+        ArrowRight,
+        Menu,
+        Plus,
+        RotateCw,
+        Search,
+    } from "lucide-svelte";
+    import ToolbarButton from "./ToolbarButton.svelte";
+    import Tab from "./Tab.svelte";
+    import { activeTab, tabs } from "../../../store/ridge";
+    import HomePage from "./HomePage.svelte";
+
+    function randomID() {
+        return (
+            Math.random().toString(36).substring(2, 15) +
+            Math.random().toString(36).substring(2, 15)
+        );
+    }
+
+    function newTab() {
+        tabs.update((currentTabs) => [
+            ...currentTabs,
+            {
+                title: "New tab",
+                id: randomID(),
+                page: { content: HomePage },
+            },
+        ]);
+    }
+</script>
+
+<div class="w-full bg-neutral-700 h-20">
+    <div class="w-full h-2/5 pt-1 px-1 gap-1 text-neutral-200 flex">
+        <div class="flex gap-1 grow-1 overflow-auto">
+            {#each $tabs as tab (tab)}
+                <Tab {tab} />
+            {/each}
+        </div>
+        <ToolbarButton on:click={newTab}>
+            <Plus />
+        </ToolbarButton>
+    </div>
+    <div class="w-full h-3/5 flex items-center px-3 py-1 gap-2">
+        <div class="flex gap-1 text-neutral-400">
+            <ToolbarButton>
+                <ArrowLeft />
+            </ToolbarButton>
+            <ToolbarButton>
+                <ArrowRight />
+            </ToolbarButton>
+            <ToolbarButton>
+                <RotateCw />
+            </ToolbarButton>
+        </div>
+        <div
+            class="w-full bg-neutral-600 rounded-full text-white p-1 flex items-center focus-within:outline focus-within:outline-2 focus-within:outline-orange-400"
+        >
+            <Search />
+            <input
+                class="bg-transparent w-full px-3 focus:outline-none"
+                placeholder="Search or go anywhere..."
+                bind:value={$activeTab.url}
+            />
+        </div>
+        <ToolbarButton>
+            <Menu class="text-neutral-400" />
+        </ToolbarButton>
+    </div>
+</div>
