d025af2aaf0ea2e5d1e57b863195a6c709d1aac9
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 02:31:53 +0100

    feat(laptop/ui): Created Tab

---
+++ iggy-laptop/web/src/apps/ridge/components/Tab.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { Mountain, X } from "lucide-svelte";
+    import { slide } from "svelte/transition";
+    import type { Tab } from "../types";
+    import { activeTab, tabs } from "../../../store/ridge";
+
+    export let tab: Tab;
+</script>
+
+<button
+    class={`p-1 ${$activeTab.id === tab.id ? "bg-neutral-600" : "bg-neutral-700 "} rounded-t-lg text-nowrap min-w-28 w-52 flex items-center gap-1 overflow-hidden transition-colors hover:bg-neutral-600`}
+    in:slide={{ axis: "x", duration: 250 }}
+    out:slide={{ axis: "x", duration: 250 }}
+    on:click={() => ($activeTab = tab)}
+>
+    <Mountain size={20} class="text-orange-400" />
+    <span class="w-min overflow-hidden">{tab.title}</span>
+    <button
+        class="ml-auto rounded-md hover:bg-neutral-500 aspect-square"
+        on:click={() => {
+            tabs.update((currentTabs) => {
+                return currentTabs.filter((t) => t.id !== tab.id);
+            });
+        }}
+    >
+        <X size={20} />
+    </button>
+</button>
