ee9b50bbcee1ac0dcb143ac125ea7073b06f5ab6
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 15:24:55 +0100

    feat(ridge): Created ridge resource

---
+++ iggy-ridge/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version("cerulean")
+
+game("gta5")
+
+name("iggy-ridge")
+
+client_script("dist/client/*.client.js")
+
+server_script("dist/server/*.server.js")
+
+dependencies({
+	"iggy-laptop",
+})
