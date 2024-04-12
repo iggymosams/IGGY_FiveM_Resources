6ccfb3f33c8685d668ce6d260c1e1dcaec7a2bde
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 16:10:16 +0100

    feat(ridge): Added basic page loading

--- iggy-ridge/client/client.ts
+++ iggy-ridge/client/client.ts
@@ -1,1 +1,21 @@
-// Start coding!
+global.exports["iggy-laptop"].RegisterLaptopCallback(
+    "ridge:searchURL",
+    async (url: string) => {
+        let data = await global.exports["iggy-utils"].TriggerQBCallBack(
+            "iggy-ridge:cb:searchURL",
+            url
+        );
+        return data;
+    }
+);
+
+global.exports["iggy-laptop"].RegisterLaptopCallback(
+    "ridge:isSiteOwner",
+    async (url: string) => {
+        let data = await global.exports["iggy-utils"].TriggerQBCallBack(
+            "iggy-ridge:cb:isSiteOwner",
+            url
+        );
+        return data;
+    }
+);
--- iggy-ridge/server/server.ts
+++ iggy-ridge/server/server.ts
@@ -1,1 +1,51 @@
-// Start coding!
+import { Player, Server } from "@zerio2/qbcore.js";
+import { oxmysql as MySQL } from "@overextended/oxmysql";
+
+const QBCore: Server = global.exports["qb-core"].GetCoreObject();
+
+let tlds = [".org", ".dev", ".com"];
+
+function containsTLD(url: string): boolean {
+    const regex = new RegExp(tlds.join("|").replace(/\./g, "\\.") + "$", "i");
+    return regex.test(url);
+}
+
+QBCore.Functions.CreateCallback(
+    "iggy-ridge:cb:searchURL",
+    async (src, cb: (data: any) => void, args: unknown[]) => {
+        let url: string = args[0] as string;
+        let isUrl = containsTLD(url);
+        if (isUrl) {
+            let data = await MySQL.query(
+                "SELECT * FROM `iggy_ridge_website` WHERE `url` = ?",
+                [url]
+            );
+            console.log(data[0]);
+            let site = {
+                title: data[0].title,
+                url: data[0].url,
+                page: {
+                    template: data[0].template,
+                    data: data[0].data,
+                },
+            };
+            cb(site);
+        }
+        cb(url);
+    }
+);
+
+QBCore.Functions.CreateCallback(
+    "iggy-ridge:cb:isSiteOwner",
+    async (src, cb: (data: any) => void, args: unknown[]) => {
+        let player: Player = QBCore.Functions.GetPlayer(src);
+
+        let url: string = args[0] as string;
+
+        let data = await MySQL.query(
+            "SELECT owner FROM `iggy_ridge_website` WHERE `url` = ?",
+            [url]
+        );
+        cb(player.PlayerData.citizenid === data[0].owner);
+    }
+);
