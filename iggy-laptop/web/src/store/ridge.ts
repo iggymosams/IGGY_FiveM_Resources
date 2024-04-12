289024e4d839f113f8a871c04dcb1ac202ee7b99
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 02:32:46 +0100

    feat(laptop/ui): Created Ridge Stores

---
+++ iggy-laptop/web/src/store/ridge.ts
@@ -0,0 +1,9 @@
+import { writable } from "svelte/store";
+import type { Tab } from "../apps/ridge/types";
+import HomePage from "../apps/ridge/components/HomePage.svelte";
+
+export const tabs = writable<Tab[]>([
+    { title: "New tab", id: "Default Tab", page: { content: HomePage } },
+]);
+
+export const activeTab = writable<Tab>();
