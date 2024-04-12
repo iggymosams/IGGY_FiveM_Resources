b82d9c334a32b4c8383d3c551ce381d7a1a53f6c
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2024-04-12 18:07:50 +0100

    feat(laptop/ui): Added ridge.com redirect to homepage

--- iggy-laptop/web/src/apps/ridge/utils.ts
+++ iggy-laptop/web/src/apps/ridge/utils.ts
@@ -5,11 +5,26 @@ import Test from "./components/templates/Test.svelte";
 import { get } from "svelte/store";
 import type { SearchResult, Site } from "./types";
 import SearchPage from "./components/SearchPage.svelte";
+import HomePage from "./components/HomePage.svelte";
 
 export async function searchURL(url: string, tab: string) {
     progress.set(90);
     let pageData: Site | SearchResult[] | undefined = undefined;
+    if (url === "ridge.com") {
+        tabs.update((currentTabs) => {
+            let i = currentTabs.findIndex((t) => t.id === tab);
+            currentTabs[i].title = "Ridge";
+            currentTabs[i].url = "ridge.com";
+            currentTabs[i].page.content = HomePage;
 
+            return currentTabs;
+        });
+
+        activeTab.set(get(tabs).find((t) => t.id === tab) || get(tabs)[0]);
+        progress.set(100);
+        setTimeout(() => progress.set(0), 2000);
+        return;
+    }
     try {
         let resp = await fetchNui("ridge:searchURL", url);
         pageData = resp;
@@ -21,7 +36,6 @@ export async function searchURL(url: string, tab: string) {
     }
     if (pageData === undefined) return;
     if (Array.isArray(pageData)) {
-        console.log(pageData);
         tabs.update((currentTabs) => {
             let i = currentTabs.findIndex((t) => t.id === tab);
             currentTabs[i].title = "Search";
